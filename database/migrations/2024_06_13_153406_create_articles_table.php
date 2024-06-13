514bfcb8bd37fb6ea5c8de924561f52e00362ba0
Marco De Vito <marco.devito93@gmail.com>
2024-06-13 17:53:47 +0200

    sistemati parametri per migrazione e migrazione effettuata

--- database/migrations/2024_06_13_153406_create_articles_table.php
+++ database/migrations/2024_06_13_153406_create_articles_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('subtitle');
+            $table->longText('content');
+          
             $table->timestamps();
         });
     }
