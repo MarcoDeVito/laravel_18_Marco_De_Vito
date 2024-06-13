50aef91c9b423cdba36a49586b8eebc0417e179f
Marco De Vito <marco.devito93@gmail.com>
2024-06-13 17:38:18 +0200

    model Article creato, routes resorces.

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('articles.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+   
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Article $article)
+    {
+        return view('articles.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Article $article)
+    {
+        return view('articles.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+   
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('homepage');
+
+Route::resource('articles',ArticleController::class);
+
 
 
